a07854a58e235ebe63b0309a112ba0714074356f
Bryanlby <liubr@oocl.com>
2019-07-25 09:39:01 +0800

    11

--- batch_file_rename.py
+++ batch_file_rename.py
@@ -1,39 +1,41 @@
 # -*- coding: GBK -*-
-# batch_file_split_rename.py
+# batch_file_rename.py
 
 import os
 import argparse
 
 
-def batch_rename(work_dir, split_name):
+def batch_rename(work_dir, replace_name):
 
     for filename in os.listdir(work_dir):
-        split_file = filename.split(split_name)
-        tupleToList = list(split_file)
-        tupleToList.remove(tupleToList.index(split_name))
+        newFileName = filename.replace(replace_name, "_")
 
         os.rename(
             os.path.join(work_dir, filename),
-            os.path.join(work_dir, tupleToList[0])
+            os.path.join(work_dir, newFileName)
         )
 
 
-
 def get_parser():
-    parser = argparse.ArgumentParser(description='change extension of files in a working directory')
+    parser = argparse.ArgumentParser(
+        description='change extension of files in a working directory')
     parser.add_argument('work_dir', metavar='WORK_DIR', type=str, nargs=1,
                         help='the directory where to change extension')
-    parser.add_argument('split_name', metavar='SPLIT_NAME', type=str, nargs=1, help='split name')
-    return parser 
+    parser.add_argument('replace_name', metavar='REPLACE_NAME',
+                        type=str, nargs=1, help='replace name')
+    return parser
+
 
 def main():
     parser = get_parser()
     args = vars(parser.parse_args())
 
     work_dir = args['work_dir'][0]
-    split_name = args['split_name'][0]
+    replace_name = args['replace_name'][0]
+    print(args['work_dir'][0])
+    print(args['replace_name'][0])
 
-    batch_rename(work_dir, split_name)
+    batch_rename(work_dir, replace_name)
 
 
 if __name__ == '__main__':
